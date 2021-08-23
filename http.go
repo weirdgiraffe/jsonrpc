95e0468aaa16a8749afca022663d37fb41483846
weirdgiraffe <giraffe@cyberzoo.xyz>
2021-08-23 13:40:04 +0200

    add http client to jsonrpc

---
+++ http.go
@@ -0,0 +1,83 @@
+package jsonrpc
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/pkg/errors"
+	"gitlab.com/glassnode/jsonrpc"
+)
+
+type ClientHTTP struct {
+	baseURL string
+	http    *http.Client
+}
+
+func NewClientHTTP(url string) *ClientHTTP {
+	return &ClientHTTP{
+		baseURL: url,
+		http:    &http.Client{Timeout: 30 * time.Second},
+	}
+}
+
+func (c *ClientHTTP) Do(ctx context.Context, req *Request) (*Response, error) {
+	data, err := json.Marshal(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to encode request")
+	}
+	r, err := http.NewRequest(
+		http.MethodPost,
+		c.baseURL,
+		bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	r.Header.Set("content-type", "application/json")
+
+	res, err := c.http.Do(r.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	jres, err := DecodeFrom(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return jres.(*Response), nil
+}
+
+func (c *ClientHTTP) DoBatch(ctx context.Context, req []*Request) ([]*Response, error) {
+	data, err := json.Marshal(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to encode request")
+	}
+	r, err := http.NewRequest(
+		http.MethodPost,
+		c.baseURL,
+		bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	r.Header.Set("content-type", "application/json")
+
+	res, err := c.http.Do(r.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	jres, err := DecodeFrom(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	l := jres.([]interface{})
+	out := make([]*Response, len(l))
+	for i := range l {
+		out[i] = l[i].(*jsonrpc.Response)
+	}
+	return out, nil
+}
